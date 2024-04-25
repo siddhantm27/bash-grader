dc095d73af2b65acc7affc85febbc2fce109f91f
siddhantm27 <smulkikar@gmail.com>
2024-04-25 23:10:45 +0530

    percentiles added report card

--- py_scripts/report_card.py
+++ py_scripts/report_card.py
@@ -2,8 +2,9 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import csv
+from grade import percentiles
 
-def generate_report_card(name,roll_no,exams,student_marks):
+def generate_report_card(name,roll_no,exams,student_marks,percentiles_list):
     img = Image.open('report_template.png')
 
     I1 = ImageDraw.Draw(img)
@@ -13,11 +14,12 @@ def generate_report_card(name,roll_no,exams,student_marks):
     # Add Text to an image
     I1.text((500, 430), f"{name}", font=myFont, fill =(0, 0, 0))
     I1.text((320, 550), f"{roll_no}", font=myFont, fill =(0, 0, 0))
-    x=350
+    x=300
     y=850
     for i in range(len(exams)):
         I1.text((x, y), f"{exams[i]}", font=myFont, fill =(0, 0, 0))
-        I1.text((x+500, y), f"{student_marks[i]}", font=myFont, fill =(0, 0, 0))
+        I1.text((x+420, y), f"{student_marks[i]}", font=myFont, fill =(0, 0, 0))
+        I1.text((x+700, y), f"{percentiles_list[i]}", font=myFont, fill =(0, 0, 0))
         y+=100
     # Display edited image
     # img.show()
@@ -32,15 +34,32 @@ with open("./main.csv", mode ='r') as csv_file:
         roll_no=row[0]
         if marks.line_num==1:
             for i in range(2,len(row)):
-                exams.append(row[i].capitalize())
+                exams.append(row[i])
         else:
             student_marks=[]
             for i in range(2,len(row)):
                 if row[i]=="a":
-                    student_marks.append(0)
+                    student_marks.append("Absent")
                 else:
                     student_marks.append(float(row[i]))
-            generate_report_card(name,roll_no,exams,student_marks)
+            old_perc_list=percentiles(roll_no)
+            percentiles_list=[]
+            old_perc_dict=dict(old_perc_list)
+
+            for exam in exams:
+                filename=exam+".csv"
+                percentile = old_perc_dict[filename]
+                if percentile=="Absent":
+                    percentiles_list.append("Absent")
+                else:
+                    percentiles_list.append(percentile.round(2))
+                    
+            # if roll_no=="23B0942":
+            #     print(old_perc_dict)
+            #     print(exams)
+            #     print(percentiles_list)
+            
+            generate_report_card(name,roll_no,exams,student_marks,percentiles_list)
     
 
 # generate_report_card("John Doe","1234",["Maths","Science","English"],[90,80,70])
